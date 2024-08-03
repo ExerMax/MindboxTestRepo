c8ff5ea257d87faa23e10c32ca9df443ec3e38a4
ExerMax <145705503+ExerMax@users.noreply.github.com>
2024-08-03 22:56:44 +0400

    New Figures
    Added new classes.
    
    Figure - abstract figure.
    
    Circle - props, constructor and Area() method.
    
    Triangle - props, constructor and methods: Area(), IsRightAngled() and private GetHypotenuse.

---
+++ MindboxTest/Geometry/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindboxTest.Geometry
+{
+    public class Circle : Figure
+    {
+        private uint Radius { get; }
+        private const double P = 3.14d;
+
+        public Circle(uint radius)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            double area = Radius * Radius * P;
+
+            return area;
+        }
+    }
+}
---
+++ MindboxTest/Geometry/Triangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindboxTest.Geometry
+{
+    internal class Triangle : Figure
+    {
+        private uint A { get; }
+        private uint B { get; }
+        private uint C { get; }
+
+        public Triangle(uint a, uint b, uint c)
+        {
+            if(GetHypotenuse(a, b, c) * 2 < (a + b + c))
+            {
+                A = a;
+                B = b;
+                C = c;
+            }
+        }
+
+        public override double Area()
+        {
+            double p = (A + B + C) / 2;
+
+            double area = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+
+            return area;
+        }
+
+        public bool IsRightAngled()
+        {
+            uint hypotenuse = GetHypotenuse(A, B, C);
+
+            if ((A*A + B*B + C*C) == (hypotenuse*hypotenuse*2))
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        private uint GetHypotenuse(uint a, uint b, uint c)
+        {
+            return Math.Max(Math.Max(a, b), c);
+        }
+    }
+}
