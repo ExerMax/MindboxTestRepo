e1feb8ddef80f952c41f97b8ec4575226887d639
ExerMax <145705503+ExerMax@users.noreply.github.com>
2024-09-17 02:48:06 +0400

    Update CircleTests

--- MindboxTest.Tests/UnitTests/CircleTests.cs
+++ MindboxTest.Tests/UnitTests/CircleTests.cs
@@ -6,16 +6,6 @@ namespace MindboxTest.Tests.UnitTests
     public class CircleTests
     {
         [Fact]
-        public void GetAreaWithZeroRadius()
-        {
-            Figure figure = new Circle(0);
-
-            var res = figure.Area();
-
-            Assert.Equal(0, res);
-        }
-
-        [Fact]
         public void GetAreaWithSomeRadius()
         {
             double someRadius = 1000.12345;
